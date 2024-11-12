aff2dabe14064191e23e05f7b2ae79d02c99d4cb
yungwine <cyrbatoff@gmail.com>
2024-11-12 19:24:13 +0400

    update complints posting time

--- mytoncore/functions.py
+++ mytoncore/functions.py
@@ -572,7 +572,7 @@ def post_complaints(local, ton):
     config32 = ton.GetConfig32()
     end = config32.get("endWorkTime")
     ts = get_timestamp()
-    if not(end + 600 <= ts < end + 1200):  # send complaints only once after the round end
+    if not(end + 1200 <= ts < end + 1800):  # send complaints only once after the round end
         return
     send_complaints(ton)
 
